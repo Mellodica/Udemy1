59031eeac50b2c2fcd1ea51e56d33ef2b2b05c7d
Helber Oliveira <mellodica.louise@gmail.com>
2020-04-15 18:11:01 -0300

    CRUD

---
+++ Data/TudoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Udemy1.Models;
+
+    public class TudoContext : DbContext
+    {
+        public TudoContext (DbContextOptions<TudoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Udemy1.Models.Departamento> Departamento { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace Udemy_1
 {
@@ -33,6 +34,9 @@ namespace Udemy_1
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TudoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TudoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
